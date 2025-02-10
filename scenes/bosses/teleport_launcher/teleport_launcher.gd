d9c6e477e014a408ad917a91cf1a3aa3f2faeee6
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2025-02-10 21:17:48 +0100

    fix missile launch and teleport bug

--- scenes/bosses/teleport_launcher/teleport_launcher.gd
+++ scenes/bosses/teleport_launcher/teleport_launcher.gd
@@ -5,20 +5,57 @@ var player:CharacterBody3D
 
 var nrOfMissiles : int = 6
 var canTeleport : bool = false
+var isTeleporting: bool = false
 
 @onready var radar: Node3D = $Radar
+@onready var teleportDelay: Timer = $TeleportDelay
+@onready var main : Node3D = get_tree().get_root().get_node("Main")
 
+const SELF_RESPAWN = preload("res://scenes/bosses/teleport_launcher/teleport_launcher.tscn")
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
 func _process(delta: float) -> void:
-	if nrOfMissiles <= 0:
-		if !canTeleport:
-			canTeleport = true
 	rotateTowardsPlayer()
+	manageState()
 
 func rotateTowardsPlayer():
 	player = get_tree().get_first_node_in_group("player")
 	if player:
 		look_at_from_position(position, player.position)
+
+func manageState():
+	if !canTeleport && nrOfMissiles <= 0:
+		canTeleport = true
+		var rnd_time : float = randf_range(1.5,3.0)
+		teleportDelay.start(rnd_time)
+
+func _on_teleport_delay_timeout():
+	teleport()
+
+func teleport() :
+	if isTeleporting:
+		return
+	isTeleporting = true
+	canTeleport=false
+	var rng_x : float = randf_range(1.0, 2.0) * 5.0 * randomSign(50)
+	var rng_z : float = randf_range(1.0, 2.0) * 5.0 * randomSign(50)
+	var position_offset : Vector3 = Vector3(rng_x, 0.0, rng_z)
+	var target_location : Vector3 = player.position + position_offset
+	var instance : CharacterBody3D = SELF_RESPAWN.instantiate()
+	instance.position = target_location
+	
+	var missileArray : Array = get_tree().get_nodes_in_group("boss_teleporter_launcher_missile")
+	for missile in missileArray:
+		queue_free()
+	main.add_child.call_deferred(instance)
+	await get_tree().create_timer(0.5).timeout
+	queue_free()
+	isTeleporting = false
+
+func randomSign(plusChance : int) -> int:
+	var rndSign : int = randi() % 100
+	if rndSign <= plusChance:
+		return 1
+	return -1
--- scenes/projectiles/homing_missile/missile.gd
+++ scenes/projectiles/homing_missile/missile.gd
@@ -1,23 +1,28 @@
 extends CharacterBody3D
 
 @export var speed : float = 5.0
+@export var launchDelay : float = 1.0
+@export var launchOffset : float = 1.0
+
 
 var direction : Vector3 
 var camera : Camera3D
 var launchTimer: Timer
+var missileArray: Array
 var launched: bool = false
 
 
 @onready var main : Node3D = get_tree().get_root().get_node("Main")
-@onready var launcher : CharacterBody3D = get_tree().get_first_node_in_group("boss_teleport_launcher")
-
+var launcher : CharacterBody3D
 const TARGET = preload("res://scenes/projectiles/homing_missile/missile_target.tscn")
+
 
 func _ready() -> void:
 	camera = get_tree().get_first_node_in_group("camera")
-	
+	launcher = get_tree().get_first_node_in_group("boss_teleport_launcher")
 	launchTimer = $LaunchTimer
 	startTimer()
+
 
 func _physics_process(delta: float) -> void:
 	if launched:
@@ -25,12 +30,13 @@ func _physics_process(delta: float) -> void:
 		velocity = direction * speed
 		reparent_to_main(self)
 		move_and_collide(velocity * delta)
+		launcher = get_tree().get_first_node_in_group("boss_teleport_launcher")
+		if camera && !is_visible_from_camera(camera):
+			launcher.nrOfMissiles -= 1
+			spawnTarget()
+			queue_free() 
+	
 
-func _process(_delta):
-	if camera && !is_visible_from_camera(camera):
-		launcher.nrOfMissiles -= 1
-		spawnTarget()
-		queue_free() 
 
 
 func is_visible_from_camera(camera: Camera3D) -> bool:
@@ -38,13 +44,14 @@ func is_visible_from_camera(camera: Camera3D) -> bool:
 
 
 func startTimer():
-	if launchTimer.is_stopped():
-		var time: float = randf_range(.25,2.5)*4.0
-		launchTimer.start(time)
-
+	launchDelay
+	launchTimer.stop()
+	launchTimer.start(launchDelay+launchOffset)
 
 func _on_launch_timer_timeout() -> void:
+	print("Timer timeout for missile:", name, " at time:", Time.get_ticks_msec())
 	launched = true
+
 
 func reparent_to_main(node: Node3D):
 	if node.get_parent():  
@@ -52,6 +59,7 @@ func reparent_to_main(node: Node3D):
 		node.get_parent().remove_child(node)  # Detach from current parent
 		main.add_child(node)  # Add to new parent
 		node.global_transform = global_transform_cache  # Restore transform
+
 
 func spawnTarget() -> void:
 	var instance : Node3D = TARGET.instantiate() 
